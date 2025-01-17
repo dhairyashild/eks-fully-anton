7ae95bf5c018ee13360b4c613d5f1dc95d489778
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2025-01-17 20:00:36 +0530

    Create 8b-csi-driver-addon.tf

---
+++ 8b-csi-driver-addon.tf
@@ -0,0 +1,11 @@
+resource "aws_eks_addon" "csi_driver" {
+  cluster_name             = aws_eks_cluster.demo.name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.11.4-eksbuild.1"
+  service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
+}
+
+
+### addon version need to update by urself
+##  aws eks describe-addon-versions--addon-name aws-ebs-csi-driver
+##above command gives addon version
