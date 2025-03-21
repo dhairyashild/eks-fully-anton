28c6afaa7994c34763596105af6ada8272f7a519
dhairyashild <125667366+dhairyashild@users.noreply.github.com>
2025-03-21 23:31:11 +0530

    Update 1a-vpc.tf

--- 1a-vpc.tf
+++ 1a-vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 # With enable_dns_support = true, the web server can resolve domain names to IP addresses, allowing it to access other services and the internet.
 # With enable_dns_hostnames = true, the web server is assigned a public DNS hostname (e.g., ec2-203-0-113-25.compute-1.amazonaws.com), which you can use to access the server from your browser or other services.
 #   cidr_block           = "10.0.0.0/16"           # it should not clash with other vpc
-
+# /16 = 65000 ip              /19= 8000 ip aprox
 
 
 
