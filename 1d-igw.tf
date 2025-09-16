864a2395366bce3cdf82d9bbc0348a5e101f85d1
dhairyashild <dhairyashild300@gmail.com>
2025-09-16 10:14:01 +0530

    Update 1d-igw.tf

--- 1d-igw.tf
+++ 1d-igw.tf
@@ -7,11 +7,11 @@ resource "aws_internet_gateway" "gw" {
 }
 
 
-Differences Between Managed by EKS and Self-Managed Kubernetes deals on Control Plane Management only:
-Managed by EKS: 
-AWS fully manages the control plane, including components like the API server and etcd,
-ensuring high availability, automatic scaling, and regular updates without user intervention.
+#Differences Between Managed by EKS and Self-Managed Kubernetes deals on Control Plane Management only:
+#Managed by EKS: 
+#AWS fully manages the control plane, including components like the API server and etcd,
+#ensuring high availability, automatic scaling, and regular updates without user intervention.
 
-Self-Managed Kubernetes: 
-Users are responsible for setting up, maintaining, and managing the control plane, 
-which includes handling upgrades, scaling, and ensuring high availability.
+#Self-Managed Kubernetes: 
+#Users are responsible for setting up, maintaining, and managing the control plane, 
+#which includes handling upgrades, scaling, and ensuring high availability.
